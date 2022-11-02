b22e984017ed5f4c4ec469c4784469f6bdae6800
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2022-11-03 02:01:21 +0900

    取り敢えず機能を追加してみた

--- src/lazyperson0710/OrganizeImageData/OrganizeImage.php
+++ src/lazyperson0710/OrganizeImageData/OrganizeImage.php
@@ -32,9 +32,11 @@ class OrganizeImage extends App {
         $count = ImageFileManagement::getInstance()->countImage();
         $this->line("確認処理が終了しました");
         $this->line("現在の画像の枚数は" . $count . "枚です");
+        //処理はすべて最後にまとめる感じで
+        $this->line("以下の質問で不正の値が入力された場合否定として処理を続行しますのでご注意ください");
         $this->line("========================================");
-        $question2 = $this->answerConfirmation($this->ask(implode(",", ImageFileManagement::Extensions) . "以外のファイルを削除しますか？(y/n): "), $type = 1);
-        if ($question2 === "yes") {
+        $result = $this->answerConfirmation($this->ask(implode(",", ImageFileManagement::Extensions) . "以外のファイルを削除しますか？(y/n): "), 1);
+        if ($result) {
             $this->line(implode(",", ImageFileManagement::Extensions) . "以外のファイル削除を開始しました...");
             $count = ImageFileManagement::getInstance()->deleteOtherFile();
             $this->line(implode(",", ImageFileManagement::Extensions) . "以外のファイルを削除し、" . $count . " 個のファイルが削除されました");
@@ -42,8 +44,8 @@ class OrganizeImage extends App {
             $this->line("ファイル削除をキャンセルしました");
         }
         $this->line("========================================");
-        $question3 = $this->answerConfirmation($this->ask("最上ディレクトリ以外のディレクトリを内部ファイルを移動した後に全て削除し最上ディレクトリにファイルを全て集めてもよろしいですか？(y/n): "), $type = 1);
-        if ($question3 === "yes") {
+        $result = $this->answerConfirmation($this->ask("最上ディレクトリ以外のディレクトリを内部ファイルを移動した後に全て削除し最上ディレクトリにファイルを全て集めてもよろしいですか？(y/n): "), 1);
+        if ($result) {
             $this->line("ファイル移動を開始しました...");
             $count = ImageFileManagement::getInstance()->moveFile();
             ImageFileManagement::getInstance()->emptyDirectoryDelete();
@@ -52,20 +54,45 @@ class OrganizeImage extends App {
             $this->line("ファイル移動をキャンセルしました");
         }
         $this->line("========================================");
-        $this->line("画像をどのように整理しますか？");
-        $this->line("1. 年 月 日 までフォルダを作成");
-        $this->line("2. 年 月 までフォルダを作成");
-        $this->line("3. 年 までフォルダを作成");
-        $number = $this->ask("番号を入力してください: ");
+        $result = $this->answerConfirmation($this->ask("入力した値以下のファイルサイズの画像を削除しますか？削除しない場合は0を入力してください(単位:kb): "), 3);
+        if ($result) {
+        } else {
+            $this->line("ファイル移動をキャンセルしました");
+        }
+        organizingType(false);
+        function organizingType(bool $recursion): void {
+            (new OrganizeImage)->line("========================================");
+            if ($recursion) {
+                (new OrganizeImage)->line("Error : 不明な値が入力された為再入力を要求します");
+            }
+            (new OrganizeImage)->line("画像をどのように整理しますか？");
+            (new OrganizeImage)->line("1. 年 月 日 までフォルダを作成");
+            (new OrganizeImage)->line("2. 年 月 までフォルダを作成");
+            (new OrganizeImage)->line("3. 年 までフォルダを作成");
+            $result = (new OrganizeImage)->answerConfirmation((new OrganizeImage)->ask("番号を入力してください: "), 2);
+            if ($result) {
+                return;
+            } else {
+                organizingType(true);
+            }
+        }
+
+        $this->line("ソート処理を開始しました");
     }
 
-    public function answerConfirmation(string $question, int $type): string {
+    public function answerConfirmation(string $question, int $type): bool|int {
         switch ($type) {
             case 1:
                 if (($question === 'y' || $question === 'yes')) {
-                    return "yes";
+                    return true;
                 } else {
-                    return "no";
+                    return false;
+                }
+            case 3:
+                if (is_numeric($question)) {
+                    return (int)$question;
+                } else {
+                    return 0;
                 }
             default:
                 throw new Error("typeが不正です");
